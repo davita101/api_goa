c6999e871649e71e8fae6ddfa3387f3ee0958926
davta101 <chemsfexs@gmail.com>
2024-06-08 02:01:14 +0400

    dix API and add comments in js

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-const city = document.getElementById("city").value
-const cityMain = document.getElementById("cityMain").innerHTML
-
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city && "tbilisi"}&appid=8005e0a1974f974d81c999400dd12dc9`)
-    .then(res => res.json())
-    .then(res => console.log(res))
-// temp , deg , weather[clear] description1
-
+// get all util document elements
+const city = document.getElementById("city")
+const cityMain = document.getElementById("cityMain")
+const icon = document.getElementById("icon")
 const btn = document.getElementById("btn")
 const value = document.querySelector("input")
-console.log(value)
+const deg = document.getElementById("deg")
+const desc = document.getElementById("desc")
+
+// when client hit submit button change weather were it si located
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
+    // fetching api 
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=8005e0a1974f974d81c999400dd12dc9`)
+        // convert our response to json file to make it file read more easy
+        .then(res => res.json())
+        //  after compiling to json we can define our temperature, describe day and also see weather logo
+        .then(res => {
+            deg.innerHTML = `${Math.round(Number(res.main.temp)) - 273}°C`
+            desc.innerHTML = "Clouds"
+            cityMain.innerHTML = city.value
+            if (res.weather[0].main == "Clouds") {
+                icon.innerHTML = "☁️"
+            }
+            else if (res.weather[0].main == "Clear") {
+                icon.innerHTML = "☀️"
+            }
+            else if (res.weather[0].main == "Rain") {
+                icon.innerHTML = "🌧️"
+            }
+            console.log(res)
+        })
+        .catch(err => "this is error my dear client try fetch again...")
+})
+
+// temp , deg , weather[clear] description1
+
 
